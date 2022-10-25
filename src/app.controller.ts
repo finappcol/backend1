199f409cbf635e5f1a6470a30f01df96689ffd85
Andrea Lisbeth Ramirez Camelo <andrea.ramirez@bayport.com.co>
2022-10-24 22:51:23 -0500

    Complia ya

---
+++ src/app.controller.ts
@@ -0,0 +1,39 @@
+
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User as UserModel} from '@prisma/client';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly userService: UserService   
+  ) {}
+
+  //   @Post('user')
+  // async signupUser(
+  //   @Body() userData: { name?: string; email: string },
+  // ): Promise<UserModel> {
+  //   return this.userService.createUser(userData);
+  // }
+
+  // @Put('publish/:id')
+  // async publishPost(@Param('id') id: string): Promise<PostModel> {
+  //   return this.postService.updatePost({
+  //     where: { id: Number(id) },
+  //     data: { published: true },
+  //   });
+  // }
+
+  // @Delete('post/:id')
+  // async deletePost(@Param('id') id: string): Promise<PostModel> {
+  //   return this.postService.deletePost({ id: Number(id) });
+  // }
+}
